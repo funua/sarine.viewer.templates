014befbad997454eb77486b55f3a866cd88b1bb9
Vlad <v.ponomarev@mail.synergetica.co>
2015-04-16 13:16:00 +0300

    Working on configs

--- app/widgets/1.2_Full_Trade_Widget/widgetConfig.js
+++ app/widgets/1.2_Full_Trade_Widget/widgetConfig.js
@@ -2,17 +2,24 @@ var widgetConfig = {
     title: 'Diamond22 story',
     color: {
         main: '#aaa',
-        
         // may be added other colors:
         background: '#ccc'
     },
     color_scheme: 'color-scheme-1',
-    pages: {
-        summary: {},
-        loupe: {},
-        light: {}
-    },
-    
+    pages: [
+        {
+            title:  'Summary',
+            code:   'summary' },
+        {
+            title:  'Loupe',
+            code:   'loupe' },
+        {
+            title:  'Hearts & Arrows',
+            code:   'hna'     },
+        {
+            title:  'Light',
+            code:   'light'   }
+    ],
     // other template-specific fields
     show_IGI_logo: true
 };
