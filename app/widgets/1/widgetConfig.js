2bd2eed8185737b1e2d98ef0202130376f6b8bb2
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-04-22 15:22:29 +0300

    1.1 Loupe Widget 6 Fix

---
+++ app/widgets/1/widgetConfig.js
@@ -0,0 +1,16 @@
+var widgetConfig = {
+    title: 'Diamond story',
+
+    color_scheme: 'color-scheme-2',
+    pages: [
+        {
+            code:   'loupe',
+            controls3d: 'ctrl-3d-1'
+        }
+    ],
+    customer_logo: {
+        img: '//dev.sarineplatform.com.s3.amazonaws.com/dev3/content/viewers/templates/0EXECUTE0/v3-dev/w1/img/igi-logo.png',
+        href: 'http://www.igiworldwide.com/',
+        title: 'IGI'
+    }
+};
