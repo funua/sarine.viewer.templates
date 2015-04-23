2a2fe0f5a241c928413a1131b8142c71bffa6254
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-04-23 18:56:11 +0300

    Fix

--- app/widgets/w1.2/widgetConfig.js
+++ app/widgets/w1.2/widgetConfig.js
@@ -1,17 +1,13 @@
 var widgetConfig = {
     title: 'Diamond story',
-    
     widget_brief_code: 'w2',
-
-    color_scheme: 'color-scheme-2',
-    
+    color_scheme: 'color-scheme-3',
     pages: [
         {
             code:   'loupe3d',
             controls3d: 'ctrl-3d-1'
         }
     ],
-    
     customer_logo: {
         img: '//dev.sarineplatform.com.s3.amazonaws.com/dev3/content/viewers/templates/0EXECUTE0/v3-dev/w1/img/igi-logo.png',
         href: 'http://www.igiworldwide.com/',
