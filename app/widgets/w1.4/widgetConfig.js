fb71e61b45309db0d1729920397733f785740f54
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-04-23 11:35:17 +0300

    w1.4

---
+++ app/widgets/w1.4/widgetConfig.js
@@ -0,0 +1,19 @@
+var widgetConfig = {
+    title: 'Diamond story',
+
+    color_scheme: 'color-scheme-1',
+    pages: [
+        {
+            title:  'Real view',
+            code:   'real',
+//            disableNavigation: true,
+            enableStoryline: true
+        },
+        {
+            title:  '3D Loupe',
+            code:   'loupe',
+            controls3d: 'ctrl-3d-2'
+        }
+    ],
+    customer_logo: false
+};
