0840a191c3ee2cc749c42f1c92d623a71e4827e5
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-05-19 12:19:31 +0300

    fix

--- app/js/app.js
+++ app/js/app.js
@@ -278,7 +278,6 @@
                 elements.sliderHeader.show();
             }, 600);
         }
-
         $('.popup-wrap button').attr('tabindex', '-1');
     }
 })(window, window.document, window.jQuery, window.FastClick, window.classie, window.Hammer, window.WallopSlider, window.PopupService, window.BulletNavigation, window.videoPlay);
