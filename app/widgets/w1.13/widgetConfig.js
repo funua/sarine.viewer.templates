78b60c075a08b455fa1ad630dd183080b7a04420
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-05-21 17:41:08 +0300

    add new

---
+++ app/widgets/w1.13/widgetConfig.js
@@ -0,0 +1,10 @@
+var widgetConfig = {
+    widget_brief_code: 'w13',
+    color_scheme: 'color-scheme-1',
+    pages: [
+        {
+            title:  'Loupe',
+            code:   'loupe'
+        }
+    ]
+};
---
+++ app/widgets/w1.17/widgetConfig.js
@@ -0,0 +1,17 @@
+var widgetConfig = {
+    title: 'Diamond Story',
+    widget_brief_code: 'w17',
+    color_scheme: 'color-scheme-1',
+    pages: [
+        {
+            title:  'Loupe',
+            code:   'loupe',
+            enableStoryline: true
+        },
+        {
+            title:  '3D Loupe',
+            code:   'loupe3d',
+            controls3d: 'ctrl-3d-2'
+        }
+    ]
+};
