34136162d6312dda348482009cd122139e406ad1
Cherous <r.kilimov@gmail.com>
2015-06-16 12:50:20 +0300

    add dynamic 1.19

---
+++ app/widgets/w1.19/widgetConfig.js
@@ -0,0 +1,27 @@
+var widgetConfig = {
+    title: 'Diamond Story',
+//    widget_brief_code: 'w5',
+//    color_scheme: 'color-scheme-1',
+    autoDisableSlides: true,
+    pages: [
+        {
+            title:  'Real View',
+            code:   'real',
+            disableNavigation: true,
+            enableStoryline: true
+        },
+        {
+            title:  'Loupe',
+            code:   'loupe'
+        },
+        {
+            title: 'Inscription',
+            code: 'loupeInscription'
+        },
+        {
+            title:  '3D Loupe',
+            code:   'loupe3d',
+            controls3d: 'ctrl-3d-2'
+        }
+    ]
+};
