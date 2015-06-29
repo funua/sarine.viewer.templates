360ab60ba7eae821dad45896971282f051a1494e
unknown <adic@AdiC-pc.sarin.local>
2015-06-29 09:38:49 +0300

    widget w1.20

---
+++ app/widgets/w1.20/widgetConfig.js
@@ -0,0 +1,23 @@
+var widgetConfig = {
+    title: 'Diamond Story',
+    widget_brief_code: 'w1',
+    color_scheme: 'color-scheme-1',
+    pages: [
+        {
+            title:  'Summary',
+            code:   'summary'
+        },
+        {
+            title:  'Light',
+            code:   'light'
+        },
+        {
+            title:  'Loupe',
+            code:   'loupe'
+        },        
+        {
+            title:  'Cut',
+            code:   'cut'
+        }
+    ]
+};
