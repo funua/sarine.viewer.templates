4cc93692509c32473c5f8cbd4a2110936e5de191
acitegrenys20 <r.kilimov@mail.synergetica.co>
2015-06-30 17:38:07 +0300

    w1.21, w1.22

---
+++ app/widgets/w1.21/widgetConfig.js
@@ -0,0 +1,27 @@
+var widgetConfig = {
+    title: 'Diamond Story',
+//    autoDisableSlides: true,
+    pages: [
+        {
+            title:  'Summary',
+            code:   'summary'
+        },
+        {
+            title:  'Light',
+            code:   'light'
+        },
+        {
+            title:  'Loupe',
+            code:   'loupe3d',
+            controls3d: 'ctrl-3d-2'
+        },
+        {
+            title:  'Hearts & Arrows',
+            code:   'hna'
+        },
+        {
+            title:  'Cut',
+            code:   'cut'
+        }
+    ]
+};
