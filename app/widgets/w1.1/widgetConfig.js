28253426eb7554e1045d45118c576d75f40cc8a1
Vlad Ponomaryov <v.ponomarev@mail.synergetica.co>
2015-07-08 12:04:01 +0300

    Added jss: modify css rules on the fly

--- app/widgets/w1.1/widgetConfig.js
+++ app/widgets/w1.1/widgetConfig.js
@@ -22,30 +22,32 @@ var widgetConfig = {
             code:   'cut'
         }
     ],
-    brandColor: "orange",
-    summaryLabel: {
-        font: "Arial",
-        size: "15px",
-        color: "red"
-    },
-    summaryValue: {
-        font: "Times New Roman",
-        size: "18px",
-        color: "green"
-    },
-    summaryNav: {
-        font: "Tahoma",
-        size: "24px",
-        color: "red"
-    },
-    conditions: {
-        font: "Tahoma",
-        size: "14px",
-        color: "darkblue"
-    },
-    poweredBy: {
-        font: "Tahoma",
-        size: "18px",
-        color: "blue"
+    styles: {
+        brandColor: "orange",
+        summaryLabel: {
+            font: "Arial",
+            size: "15px",
+            color: "red"
+        },
+        summaryValue: {
+            font: "Times New Roman",
+            size: "18px",
+            color: "green"
+        },
+        summaryNav: {
+            font: "Tahoma",
+            size: "24px",
+            color: "red"
+        },
+        conditions: {
+            font: "Tahoma",
+            size: "14px",
+            color: "darkblue"
+        },
+        poweredBy: {
+            font: "Tahoma",
+            size: "18px",
+            color: "blue"
+        }
     }
 };
